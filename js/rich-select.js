69f7f6146e5031b3fbf7e148fe364624b462e013
kato takahisa <kato@J-0339.local>
2011-12-14 14:36:43 +0900

    changed styles.

--- js/rich-select.js
+++ js/rich-select.js
@@ -18,21 +18,19 @@ Event.observe(window, 'load', function(){
         };
         var computedStyle = function(obj, name) {
                 if( obj.currentStyle ) { //IE or Opera
-                        if( name.indexOf( "-" ) != -1 ) name = name.camelize();
-                        return  obj.currentStyle[ name ];
-                } else if ( getComputedStyle ) { //Mozilla or Opera or Safari
-                        return document.defaultView.getComputedStyle( obj, '' ).getPropertyValue( name );
+                        if (name.indexOf("-") != -1) name = name.camelize();
+                        return  obj.currentStyle[name];
+                } else if (getComputedStyle) { //Mozilla or Opera or Safari
+                        return document.defaultView.getComputedStyle(obj, '').getPropertyValue(name);
                 }
                 return null;
         };
-        var selects = $$('select.rich');
-        selects.each(function(select){
+        $$('select.rich').each(function(select){
                 var text = document.createElement('input');
                 text.className = 'rich-select-text';
                 var copy_styles = $A([
                         'margin-top', 'margin-right', 'margin-bottom', 'margin-left',
-                        'padding-top', 'padding-right', 'padding-bottom', 'padding-left',
-                        'font'
+                        'padding-top', 'padding-right', 'padding-bottom', 'padding-left'
                 ]);
                 var $select = select;
                 copy_styles.each(function(elm) {
@@ -55,18 +53,28 @@ Event.observe(window, 'load', function(){
                                         alt = 'rich-select-option-alt';
                                 }
                                 o.innerText = sel.innerText;
-                                o.setAttribute('data-value', sel.getAttribute('value'));
+                                var current_value = sel.getAttribute('value');
+                                o.setAttribute('data-value', current_value);
+                                if ($F($select) == current_value) {
+                                        o.className += ' rich-select-option-current';
+                                }
                                 o.observe('click', function(){
                                         $select.value = o.getAttribute('data-value');
                                         $select.parentNode.removeChild(box);
                                         $select.style.display = $select.getAttribute('data-orgdisplay');
+                                        link.style.display = 'inline';
                                 });
                                 box.appendChild(o);
                         };
                 };
 
                 box.appendChild(text);
-                select.observe('focus', function(){
+
+                var link = document.createElement('span');
+                link.className = 'rich-select-trigger';
+                link.innerHTML = '&nbsp;&nbsp;&nbsp;';
+                link.setAttribute('title', 'searchable select interface.');
+                link.observe('click', function(ev){
                         text.observe('keyup', function(ev){
                                 var input_value = ev.target.value;
                                 $A(box.childNodes).each(function(child){
@@ -78,6 +86,7 @@ Event.observe(window, 'load', function(){
                         });
                         $select.setAttribute('data-orgdisplay', $select.style.display);
                         $select.style.display = 'none';
+                        link.style.display = 'none';
                         box.style.position = 'absolute';
                         var pos = getElementPosition($select);
                         box.style.top = pos.top;
@@ -89,6 +98,18 @@ Event.observe(window, 'load', function(){
                         });
                         $A($select.options).each(setupSelectionFunc(''));
                         $select.parentNode.appendChild(box);
+                        text.focus();
+                        Event.stop(ev);
+                        document.body.observe('click', function(ev){
+                                Event.stop(ev);
+                                if (box.parentNode == null) return;
+                                Element.remove(box);
+                                $select.style.display = $select.getAttribute('data-orgdisplay');
+                                link.style.display = 'inline';
+                        });
+                });
+                select.insert({after: link});
+                select.observe('focus', function(){
                 });
                 text.observe('click', function(elm){
                 });
@@ -97,16 +118,15 @@ Event.observe(window, 'load', function(){
         //style
         var s = document.createElement('style');
         s.innerText = '' +
-                '.rich-select {border: 1px solid gray;}' +
-                '.rich-select-option {background-color: #eee;cursor: pointer;}' +
+                '.rich-select-text {border: 1px solid gray; }' +
+                '.rich-select {height:400px; overflow: scroll;}' +
+                '.rich-select-option {background-color: #eee;cursor: pointer; font-size: 90%; padding: 0px 5px;}' +
+                '.rich-select-option-current {font-weight: bold;}' +
+                '.rich-select-option:hover {background-color: #ccf;cursor: pointer; font-size: 90%;}' +
                 '.rich-select-option-alt {background-color: #ddd;}' +
-                '' +
-                '' +
-                '' +
-                '' +
+                '.rich-select-trigger {cursor: pointer; vertical-align: middle; margin: 5px; background-color: #ccf; border: 1px solid #99c;}' +
                 '';
         document.body.appendChild(s);
 });
 
 //  vim: set ts=4 sw=4 sts=4 expandtab fenc=utf-8:
-
